752f1ed383f5bc769882405340a3db3196e386af
davidrda <davidramirez.rda@gmail.com>
2025-10-04 09:34:05 +0200

    update

--- DAW/T1/PrimerProyecto/src/Main.java
+++ DAW/T1/PrimerProyecto/src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    //Esto es un comentario
+    /*
+    este comentario se puede divir
+    en varias líneas de código
+     */
+    // TODO me falta esto
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Este es mi segunda línea programa en Java");
+    }
+
+    public void nombreMetodo(){
+
+    }
 }
